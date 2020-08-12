facb1d03fbf793b76f8705218df6a3e7f1ce43c3
Hyung-Taik Choi <htcrefactor@ajou.ac.kr>
2020-08-12 14:44:59 +0900

    Change code of 6.ptrNarray1.c

--- threads/pointer/6.ptrNarray1.c
+++ threads/pointer/6.ptrNarray1.c
@@ -6,10 +6,28 @@ int main(void)
 	short *p;
 	int i;
 
+	p = &arr[0];
+	printf("%d,%d\n", arr[1], p[3]);
+
+	p = &arr[1];
+	printf("%d,%d\n", arr[1], p[3]);
+
+#if 0
 	for(i=0, p=arr; i<5; i++) {
-		printf("%d,%d\n", arr[i], ....);
-		printf("%d,%d\n", ....., *(p+i));
+		printf("%d,%d\n", arr[i], p[i]);
+		printf("%d,%d\n", *(arr + i), *(p+i));
 	}
+#endif
+
+	for(i=0, p=arr; i<5; i++, p++) {
+		printf("%d,%d\n", arr[i], p[i]);
+	}
+	
+	/*This doens't work because arr is declared as short * const arr internally.
+	for(i=0, p=arr; i<5; i++, arr++) {
+		printf("%d,%d\n", p[i], *arr);
+	}
+	*/
 
 	return 0;
 }
