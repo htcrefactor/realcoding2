29485dea467dc19f8750b172352cc507275e1eb7
Hyung-Taik Choi <htcrefactor@ajou.ac.kr>
2020-08-12 15:41:44 +0900

    Update code for hellothreads_st.c

--- threads/1.hellothread_st.c
+++ threads/1.hellothread_st.c
@@ -1,42 +1,49 @@
+// Input: 30 int values of score data
+// Main: makes sthread1, sthread2 work and leaves
+// sthread1: avg, sum
+// sthread2: sort and print
+// man pthread_create
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-......
+#include <pthread.h>
 
-int d1=10, d2;
+int d1 = 10, d2;
 
-____ hello_thread(_______)
-{
-	int i=200;
-	
-	printf("Hello_Thread Start(d1:%d, d2:%d, i:%d)!!\n", d1,d2,i);
-	for(i=1; i<=5; i++) {
+void* hello_thread(void* arg) {
+	int i = 200;
+
+	printf("Hello_Thread Start(d1:%d, d2:%d, i:%d)!!\n", d1, d2, i);
+	for (i = 1; i <= (int)arg; i++) {
 		printf("Hello_Thread_%d!!\n", i);
 		d1++;
 		d2++;
 		sleep(1);
 	}
-	printf("Hello_Thread End(d1:%d, d2:%d, i:%d)!!\n", d1,d2,i);
+	printf("Hello_Thread End(d1:%d, d2:%d, i:%d)!!\n", d1, d2, i);
 	return arg;
 }
 
-int main(void)
-{
+int main(void) {
 	pthread_t tid;
+	pthread_attr_t attr; // Find attributes by googling.
 	int status;
-	int i=100;
+	int i = 100;
+
+	printf("Main_Thread Start(d1:%d, d2:%d, i:%d)!!\n", d1, d2, i);
+	status = pthread_create(&tid, NULL, hello_thread, (void*)5);
 	
-	printf("Main_Thread Start(d1:%d, d2:%d, i:%d)!!\n", d1,d2,i);
-	status = ________________
-	if(status != 0) {
+	if (status != 0) {
 		perror("thread create");
 		exit(1);
-	}	
-	
-	for(i=1; i<=8; i++) {
+	}
+
+	for (i = 1; i <= 8; i++) {
 		printf("Main_Thread_%d!!\n", i);
 		sleep(1);
 	}
-	printf("Main_Thread End(d1:%d, d2:%d, i:%d)!!\n", d1,d2,i);
-    return 0;
+	
+	printf("Main_Thread End(d1:%d, d2:%d, i:%d)!!\n", d1, d2, i);
+	return 0;
 }
