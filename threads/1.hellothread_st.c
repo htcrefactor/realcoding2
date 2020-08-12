a46abe755cb9c5c7c97b58196a2946ddaa9298b0
Hyung-Taik Choi <htcrefactor@ajou.ac.kr>
2020-08-12 15:49:57 +0900

    Update code for hellothreads_st.c 2

--- threads/1.hellothread_st.c
+++ threads/1.hellothread_st.c
@@ -22,7 +22,8 @@ void* hello_thread(void* arg) {
 		sleep(1);
 	}
 	printf("Hello_Thread End(d1:%d, d2:%d, i:%d)!!\n", d1, d2, i);
-	return arg;
+	//return arg;
+	pthread_exit(arg);
 }
 
 int main(void) {
@@ -32,18 +33,20 @@ int main(void) {
 	int i = 100;
 
 	printf("Main_Thread Start(d1:%d, d2:%d, i:%d)!!\n", d1, d2, i);
-	status = pthread_create(&tid, NULL, hello_thread, (void*)5);
+	status = pthread_create(&tid, NULL, hello_thread, (void*)15);
 	
 	if (status != 0) {
 		perror("thread create");
 		exit(1);
 	}
 
-	for (i = 1; i <= 8; i++) {
+	for (i = 1; i <= 5; i++) {
 		printf("Main_Thread_%d!!\n", i);
 		sleep(1);
 	}
-	
+
 	printf("Main_Thread End(d1:%d, d2:%d, i:%d)!!\n", d1, d2, i);
+	// Do not kill shared global memory so child threads can still use this data.
+	pthread_exit(NULL);
 	return 0;
 }
