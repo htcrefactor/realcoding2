5b11ded381de997ceb8ebbc53928fb3759f8cf69
Hyung-Taik Choi <htcrefactor@ajou.ac.kr>
2020-08-14 17:52:43 +0900

    Reindent LedThread.c

--- ThreadApp/ledThreads/LedThreads.c
+++ ThreadApp/ledThreads/LedThreads.c
@@ -17,79 +17,95 @@
 pthread_mutex_t mutex_id;
 #endif
 
-int main(int argc, char* argv[]) {
-    if(argc != 1 || argc != 3) {
-		printf("Usage : LedThreads.exe [GPIO_NO] [BLINK_NO]\n");
-		exit(-1);
-	}
+int main(int argc, char *argv[]) 
+{
+    if (argc != 1 || argc != 3)
+    {
+        printf("Usage : LedThreads.exe [GPIO_NO] [BLINK_NO]\n");
+        exit(-1);
+    }
 
     pthread_t tid_blue, tid_yellow;
-    void* thread_result;
+    void *thread_result;
 
 #ifndef NOMUTEX
-	if(pthread_mutex_init(&mutex_id, NULL) != 0){
-		perror("pthread_mutex_init");
-		exit(errno);
-	}
+    if (pthread_mutex_init(&mutex_id, NULL) != 0)
+    {
+        perror("pthread_mutex_init");
+        exit(errno);
+    }
 #endif
 
-    if(wiringPiSetup() == -1) {
+    if (wiringPiSetup() == -1)
+    {
         perror("wiringPiSetup()\n");
         return -1;
     }
 
-if(argc == 1) {
-	blinkLed(GPIO_WPI_RED, 10, 1);
-}
-
-else {
-    if(strcmp(argv[0], "1") == 0) {
-        if(pthread_create(&tid_blue, NULL, thread_function_blue, argv[1])!=0) {
-		    perror("pthread_create");
-		    exit(1);
-	    }
+    if (argc == 1)
+    {
+        blinkLed(GPIO_WPI_RED, 10, 1);
     }
 
-    else if(strcmp(argv[0], "2") == 0) {
-        if(pthread_create(&tid_yellow, NULL, thread_function_yellow, argv[1])!=0) {
-		    perror("pthread_create");
-		    exit(1);
-	    }
+    else
+    {
+        if (strcmp(argv[0], "1") == 0)
+        {
+            if (pthread_create(&tid_blue, NULL, thread_function_blue, argv[1]) != 0)
+            {
+                perror("pthread_create");
+                exit(1);
+            }
+        }
+
+        else if (strcmp(argv[0], "2") == 0)
+        {
+            if (pthread_create(&tid_yellow, NULL, thread_function_yellow, argv[1]) != 0)
+            {
+                perror("pthread_create");
+                exit(1);
+            }
+        }
+
+        else if (strcmp(argv[0], "3") == 0)
+        {
+            if (pthread_create(&tid_blue, NULL, thread_function_blue, argv[1]) != 0)
+            {
+                perror("pthread_create");
+                exit(1);
+            }
+
+            if (pthread_create(&tid_yellow, NULL, thread_function_yellow, argv[1]) != 0)
+            {
+                perror("pthread_create");
+                exit(1);
+            }
+
+            if (pthread_join(tid_blue, &thread_result) != 0)
+            {
+                perror("pthread_join");
+                exit(1);
+            }
+
+            if (pthread_join(tid_yellow, &thread_result) != 0)
+            {
+                perror("pthread_join");
+                exit(1);
+            }
+        }
+
+        else
+        {
+            printf("Usage : LedThreads.exe [GPIO_NO] [BLINK_NO]\n");
+            printf("[GPIO_NO] = {1: BLUE ONLY, 2: YELLOW ONLY, 3: BOTH} [BLINK_NO] = {N}\n");
+            exit(-1);
+        }
     }
 
-    else if(strcmp(argv[0], "3") == 0) {
-        if(pthread_create(&tid_blue, NULL, thread_function_blue, argv[1])!=0) {
-		    perror("pthread_create");
-		    exit(1);
-	    }
-
-        if(pthread_create(&tid_yellow, NULL, thread_function_yellow, argv[1])!=0) {
-		    perror("pthread_create");
-		    exit(1);
-	    }
-
-        if(pthread_join(tid_blue, &thread_result)!=0) {
-		    perror("pthread_join");
-		    exit(1);
-	    }
-	    
-        if(pthread_join(tid_yellow, &thread_result)!=0) {
-		    perror("pthread_join");
-		    exit(1);
-	    }
-    }
-
-    else {
-        printf("Usage : LedThreads.exe [GPIO_NO] [BLINK_NO]\n");
-        printf("[GPIO_NO] = {1: BLUE ONLY, 2: YELLOW ONLY, 3: BOTH} [BLINK_NO] = {N}\n");
-		exit(-1);
-    }
-}
-	
 #ifndef NOMUTEX
-	pthread_mutex_destroy(&mutex_id);
+    pthread_mutex_destroy(&mutex_id);
 #endif
 
-	printf("Execution Finished :: %s\n", (char *) thread_result);
-	return 0;
+    printf("Execution Finished :: %s\n", (char *)thread_result);
+    return 0;
 }
