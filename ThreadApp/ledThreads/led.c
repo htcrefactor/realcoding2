abc656cb9cc3b20a12cc2c990c36b34966e795b0
Hyung-Taik Choi <htcrefactor@ajou.ac.kr>
2020-08-14 17:48:22 +0900

    Refactor blinkLed().

---
+++ ThreadApp/ledThreads/led.c
@@ -0,0 +1,21 @@
+#include "led.h"
+
+void blinkLed(int pin, int turns, int period) {
+    char buffer[64];
+    pinMode(pin, OUTPUT);
+	for(int i = 0 ; i < turns ; i++) {
+		digitalWrite(pin, 1);
+
+        sprintf(buffer, "LED :: PIN(%d) READ(1) COUNTER(%d)\n", pin, i);
+		write(1, buffer, strlen(buffer));
+		
+        delay(period * 1000);
+		
+        digitalWrite(pin, 0);
+        
+        sprintf(buffer, "LED :: PIN(%d) READ(0) COUNTER(%d)\n", pin, i);
+		write(1, buffer, strlen(buffer));
+		
+        delay(period * 1000);
+	}
+}
--- ThreadApp/ledThreads/led.h
+++ ThreadApp/ledThreads/led.h
@@ -3,22 +3,4 @@
 #define GPIO_WPI_RED 1
 #define GPIO_PERIOD_RED 1
 
-void blinkLed(int pin, int turns, int period) {
-    char buffer[64];
-    pinMode(pin, OUTPUT);
-	for(int i = 0 ; i < turns ; i++) {
-		digitalWrite(pin, 1);
-
-        sprintf(buffer, "LED :: PIN(%d) READ(1) COUNTER(%d)\n", pin, i);
-		write(1, buffer, strlen(buffer));
-		
-        delay(period * 1000);
-		
-        digitalWrite(pin, 0);
-        
-        sprintf(buffer, "LED :: PIN(%d) READ(0) COUNTER(%d)\n", pin, i);
-		write(1, buffer, strlen(buffer));
-		
-        delay(period * 1000);
-	}
-}
+extern void blinkLed(int pin, int turns, int period);
