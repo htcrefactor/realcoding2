cec78c715b9e9a1e4f8ab8eba422ca5e3e899562
Hyung-Taik Choi <htcrefactor@ajou.ac.kr>
2020-08-14 17:50:03 +0900

    Debug include issues.

--- ThreadApp/ledThreads/led_blue.c
+++ ThreadApp/ledThreads/led_blue.c
@@ -1,9 +1,10 @@
 #define GPIO_WPI_BLUE 4
 #define GPIO_PERIOD_BLUE 2
 
+#include "led_blue.c"
 #include "led.h"
 
 void* thread_function_blue(void* arg) {
-    blinkLed(GPIO_WPI_BLUE, GPIO_PERIOD_BLUE, arg);
+    blinkLed(GPIO_WPI_BLUE, GPIO_PERIOD_BLUE, (int*)arg);
     pthread_exit("thread_blue end");
 }
--- ThreadApp/ledThreads/led_yellow.c
+++ ThreadApp/ledThreads/led_yellow.c
@@ -1,9 +1,10 @@
 #define GPIO_WPI_YELLOW 5
 #define GPIO_PERIOD_YELLOW 3
 
+#include "led_yellow.h"
 #include "led.h"
 
 void* thread_function_yellow(void* arg) {
-    blinkLed(GPIO_WPI_YELLOW, GPIO_PERIOD_YELLOW, arg);
+    blinkLed(GPIO_WPI_YELLOW, GPIO_PERIOD_YELLOW, (int*)arg);
     pthread_exit("thread_yellow end");
 }
