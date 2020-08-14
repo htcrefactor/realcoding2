986c4bcd81066e635154be28867d76200e7674e7
Hyung-Taik Choi <htcrefactor@ajou.ac.kr>
2020-08-14 17:46:53 +0900

    Include led.h to led_blue.c and led_yellow.c

--- ThreadApp/ledThreads/led_yellow.c
+++ ThreadApp/ledThreads/led_yellow.c
@@ -1,6 +1,8 @@
 #define GPIO_WPI_YELLOW 5
 #define GPIO_PERIOD_YELLOW 3
 
+#include "led.h"
+
 void* thread_function_yellow(void* arg) {
     blinkLed(GPIO_WPI_YELLOW, GPIO_PERIOD_YELLOW, arg);
     pthread_exit("thread_yellow end");
